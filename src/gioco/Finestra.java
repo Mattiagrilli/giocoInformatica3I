e1ce4bf5165ebc39e828071822d7441b3bd1395c
FrancescoFondacci <fr4nc3sc0f0nd4cc1@gmail.com>
2025-04-27 23:52:19 +0200

    Aggiunta classe Livello1, modificata classe Finestra ,aggiunte immagini tile e file txt per livello

--- src/gioco/Finestra.java
+++ src/gioco/Finestra.java
@@ -1,27 +1,29 @@
-package Gioco;
+package gioco;
 
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
 public class Finestra extends Application {
+		
 	Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
 
 	double screenWidth = screenBounds.getWidth();
 	double screenHeight = screenBounds.getHeight();
+	
 	@Override
 	public void start(Stage stage) {
-		Pane root = new Pane();
-		Scene scene = new Scene(root, screenWidth, screenHeight);
+		Livello1 livello=new Livello1();
+		Scene scene = livello.getScene();
 		stage.setScene(scene);
 		stage.setTitle("GiocoInformatica");
 		stage.show();
-
+		
 	}
 
 	public static void main(String[] args) {
 		launch();
 	}
-	}
+}
